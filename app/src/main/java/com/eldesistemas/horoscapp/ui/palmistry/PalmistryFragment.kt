383b770b960da1b6f72e6647f77cbe099d34d3ea
pedrovargas00 <01vargaspedro@gmail.com>
2024-04-07 11:48:31 -0600

    Palmistry screen completed

--- app/src/main/java/com/eldesistemas/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/eldesistemas/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,17 +1,37 @@
 package com.eldesistemas.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.eldesistemas.horoscapp.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import com.eldesistemas.horoscapp.databinding.FragmentPalmistryBinding
 
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+         if (it)
+             startCamera()
+         else
+             Toast.makeText(requireContext(), "Es necesario aceptar los permisos", Toast.LENGTH_LONG).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +39,43 @@ class PalmistryFragment : Fragment() {
     ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission())
+            startCamera()
+        else
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+    }
+
+    private fun checkCameraPermission(): Boolean {
+
+        return PermissionChecker.checkSelfPermission(
+            requireContext(), CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview
+                .Builder()
+                .build()
+                .also { it.setSurfaceProvider(binding.viewFinder.surfaceProvider) }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.e("Exception", "Falló al iniciar cámara")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 }
